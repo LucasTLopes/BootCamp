cdc8a0eb9ccf3448b3834c2721040401ca9b3c6f
Lucas Troleiz Lopes <lucas.troleiz@mercadolivre.com>
2022-03-11 17:09:33 -0300

    Criando clase Main e classe Pessoa

---
+++ src/exercicios/Main.java
@@ -0,0 +1,14 @@
+package exercicios;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Pessoa pessoa = new Pessoa();
+
+        Pessoa pessoa1 = new Pessoa("Lucas",24,"123");
+
+        Pessoa pessoa2 = new Pessoa("Karol",22,"4321",47,1.71);
+
+
+    }
+}
