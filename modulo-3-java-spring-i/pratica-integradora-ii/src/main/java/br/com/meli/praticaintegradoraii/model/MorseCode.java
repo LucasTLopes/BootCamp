e5c4024fd7e9799e736756dc1e2ddd409672bd0f
Lucas Troleiz Lopes <lucas.troleiz@mercadolivre.com>
2022-03-29 10:57:33 -0300

    add : project to include example about unitary tests

---
+++ modulo-3-java-spring-i/pratica-integradora-ii/src/main/java/br/com/meli/praticaintegradoraii/model/MorseCode.java
@@ -0,0 +1,17 @@
+package br.com.meli.praticaintegradoraii.model;
+
+public class codigoMorse {
+    private String codigo;
+
+    public codigoMorse(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+}
---
+++ modulo-3-java-spring-i/pratica-integradora-ii/src/main/java/br/com/meli/praticaintegradoraii/service/ConverterService.java
@@ -0,0 +1,2 @@
+package br.com.meli.praticaintegradoraii.service;public class ConverterService {
+}
